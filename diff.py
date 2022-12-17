7c37d7f66ac776bc81fb43a3355bf9782f305b0a
Joel Yang <y2101831558@gmail.com>
2022-12-17 11:39:32 +0800

    output to html

--- diff.py
+++ diff.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+from ansi2html import Ansi2HTMLConverter
 
 
 ###############################################################################
@@ -17,15 +18,21 @@ def main(argv):
     diff_dir_path = argv[1]
     diff_dir_content = os.listdir(diff_dir_path)
     diff_name_list = [diff_unit for diff_unit in diff_dir_content if ".sarif" == diff_unit[-6:]]
+
+    if diff_dir_path[-1] != '/':
+        diff_dir_path += '/'
+
+    result_str = ""
     for diff_name in diff_name_list:
         diff_name = diff_name[:-6]
-        if diff_dir_path[-1] != '/':
-            print("=== {:^30}.c ===".format(diff_name))
-            subprocess.call(
-                "python sse-assessment.py {0}/{1}.sse.db {0}/{1}.ikos.db {0}/{1}.sarif".format(diff_dir_path, diff_name))
-        else:
-            subprocess.call(
-                "python sse-assessment.py {0}{1}.db {0}{1}.ikos.db {0}{1}.sarif".format(diff_dir_path, diff_name))
+        one_result_str = "=== {:^30}.c ===".format(diff_name)+"\n"
+        one_result_str += subprocess.run("python sse-assessment.py {0}{1}.sse.db {0}{1}.ikos.db {0}{1}.sarif".format(diff_dir_path, diff_name), capture_output=True, text=True).stdout
+        result_str += (one_result_str+"\n")
+
+    conv = Ansi2HTMLConverter(dark_bg=False, markup_lines=True)
+    html = conv.convert(result_str)
+    with open("result.html", "w") as f:
+        f.write(html)
 
 
 if __name__ == "__main__":
